7a89eaf21bcac3a94c05acf1913727f9f1c95f82
Cesar <cahern92@gmail.com>
2025-05-04 22:08:06 -0700

    Organizing

---
+++ Courses/CMPINF_2100/publish/week_10/week_01/our_first_script.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Aug 25 20:32:15 2024
+
+@author: cesarhernandez
+"""
+
+x = 2
+y = 3
+
+4**2
+
+z = x + y
+
+# Hey girl hey, this line can't be evaluated 
+
+# 2 + 2
